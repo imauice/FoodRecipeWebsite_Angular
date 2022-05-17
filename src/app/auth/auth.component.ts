08ba064dcd72fcc1665e854198a90d15e28478ef
partha99saha <psaha.bdn9@gmail.com>
2022-05-17 22:00:28 +0530

    signup added

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
-  templateUrl: './auth.component.html'
+  templateUrl: './auth.component.html',
 })
 export class AuthComponent {
   isLoginMode = true;
 
-  onSwitchMode(){
-    this.isLoginMode = !this.isLoginMode; 
+  constructor(private authService: AuthService) {}
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    if(!this.isLoginMode){
+      const email = form.value.email;
+      const password = form.value.password;
+      this.authService.signup(email, password).subscribe(
+      (resData) => {
+        console.log(resData);
+      },
+      (error) => {
+        console.log(error);
+      }
+      );  
+    } else {
+      //
+    }
+
+    form.reset();
   }
 }
