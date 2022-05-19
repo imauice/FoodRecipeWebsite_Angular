327f70940e40b51b7e7fd61adc973f40221d88c6
partha99saha <psaha.bdn9@gmail.com>
2022-05-19 23:32:40 +0530

    update for auth service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -19,6 +19,7 @@ export interface AuthResponceData {
 })
 export class AuthService {
   user = new BehaviorSubject<User>(null);
+  private tokenExpirationTimer:any;
 
   constructor(
     private http: HttpClient,
@@ -70,9 +71,46 @@ export class AuthService {
       );
   }
 
+  autoLogin(){
+    const userData:{
+      email:string;
+      id:string;
+      _token:string;
+      _tokenExpirationData:string
+    } = JSON.parse(localStorage.getItem('userData'));
+    if(!userData){
+      return;
+    }
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationData)
+    );
+    if(loadedUser.token){
+      this.user.next(loadedUser);
+      const expirationDuration = new Date(userData._tokenExpirationData)
+        .getTime()- new Date().getTime();
+      this.autoLogout(expirationDuration);
+
+    }
+  }
+
   logout() {
     this.user.next(null);
     this.router.navigate(['/auth']);
+    // localStorage.removeItem('userData');
+    localStorage.clear();
+    if(this.tokenExpirationTimer){
+      clearTimeout(this.tokenExpirationTimer);
+    }
+    this.tokenExpirationTimer = null;
+  }
+
+  autoLogout(expirationDuration:number){
+    setTimeout(()=>{
+      this.logout();
+    },expirationDuration)
   }
 
   private handleAuthentication(
@@ -81,9 +119,11 @@ export class AuthService {
     token: string,
     expiresIn: number
   ) {
-    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 10000);
     const user = new User(email, userId, token, expirationDate);
     this.user.next(user);
+    this.autoLogout(expiresIn*1000);
+    localStorage.setItem('userData',JSON.stringify(user));
   }
 
   private handleError(errorRes: HttpErrorResponse) {
